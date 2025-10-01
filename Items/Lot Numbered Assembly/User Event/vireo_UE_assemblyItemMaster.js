9561af49a2d2053fcc975080971a111a49e6c942
Nagendra Babu <169066080+Nag2304@users.noreply.github.com>
2025-10-01 20:47:34 +0530

    Create vireo_UE_assemblyItemMaster.js

---
+++ Items/Lot Numbered Assembly/User Event/vireo_UE_assemblyItemMaster.js
@@ -0,0 +1,108 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType UserEventScript
+ * @NModuleScope SameAccount
+ */
+
+/**
+ * File name: vireo_UE_assemblyItemMaster.js
+ * Script: Vireo | UE Assembly Master
+ * Author           Date       Version               Remarks
+ * nagendrababu 09.28.2025     1.00        Initial creation of the script
+ *
+ */
+
+/* -------------------------- Script Usage - Begin -------------------------- */
+/**
+ * Uses .
+ */
+/* -------------------------- Script Usage - End -------------------------- */
+
+/* global define,log*/
+
+define([
+  'SuiteScripts/Kenai/Items/Assembly/Modules/vireo_Module_setLastTransferPrice',
+], (setLastTransferPrice) => {
+  /* ------------------------ Global Variables - Begin ------------------------ */
+  const exports = {};
+  /* ------------------------- Global Variables - End ------------------------- */
+  //
+  /* --------------------------- Before Load - Begin --------------------------- */
+  /**
+   *
+   * @param {object} scriptContext
+   */
+  const beforeLoad = (scriptContext) => {
+    const loggerTitle = 'Before Load';
+    log.audit(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Entry-------------------<|'
+    );
+    try {
+    } catch (error) {
+      log.error(loggerTitle + ' caught an exception', error);
+    }
+    //
+    log.audit(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Exit-------------------<|'
+    );
+  };
+  /* ---------------------------- Before Load - End ---------------------------- */
+  //
+  /* --------------------------- Before Submit - Begin --------------------------- */
+  /**
+   *
+   * @param {object} scriptContext
+   */
+  const beforeSubmit = (scriptContext) => {
+    const loggerTitle = 'Before Submit';
+    log.audit(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Entry-------------------<|'
+    );
+    try {
+    } catch (error) {
+      log.error(loggerTitle + ' caught an exception', error);
+    }
+    //
+    log.audit(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Exit-------------------<|'
+    );
+  };
+  /* ---------------------------- Before Submit - End ---------------------------- */
+  //
+  /* --------------------------- After Submit - Begin --------------------------- */
+  /**
+   *
+   * @param {object} scriptContext
+   */
+  const afterSubmit = (scriptContext) => {
+    const loggerTitle = 'After Submit';
+    log.audit(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Entry-------------------<|'
+    );
+    try {
+      if (scriptContext !== scriptContext.UserEventType.DELETE) {
+        setLastTransferPrice.afterSubmit(scriptContext);
+      }
+    } catch (error) {
+      log.error(loggerTitle + ' caught an exception', error);
+    }
+    //
+    log.audit(
+      loggerTitle,
+      '|>-------------------' + loggerTitle + ' -Exit-------------------<|'
+    );
+  };
+  /* ---------------------------- After Submit - End ---------------------------- */
+  //
+  /* ----------------------------- Exports - Begin ---------------------------- */
+  //exports.beforeLoad = beforeLoad;
+  //exports.beforeSubmit = beforeSubmit;
+  exports.afterSubmit = afterSubmit;
+  return exports;
+  /* ------------------------------ Exports - End ----------------------------- */
+});
